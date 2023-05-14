963497ab9a2441f2186aa2dad2335cb955cc8394
Arsene Kwete <tchinda.geraud@yahoo.fr>
2023-05-14 20:00:00 +0200

    change config files

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://simple-books-api.glitch.me'
+    watchForFileChanges: true,
+    chromeWebSecurity: false,
+    baseUrl: 'https://simple-books-api.glitch.me',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
